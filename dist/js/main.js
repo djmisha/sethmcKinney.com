c6bf4a61c329586f814f4c873500ac76001bdcb9
Misha Osinvoskiy <misha.osinovskiy@gmail.com>
2021-02-19 11:04:02 -0800

    daily commit

--- dist/js/main.js
+++
@@ -1,5 +0,0 @@
-/*! amex-gbt-portal v0.0.1 | (c) 2021 Lewis | MIT License | https://bitbucket.org/mishaosinovskiy/amex-gbt-portal/src/master/ */
-document.addEventListener('click', (function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}), false);
