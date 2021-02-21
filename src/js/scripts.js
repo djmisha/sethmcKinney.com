98342a20b6fb425fb9b4a4dd04458b931bb610bb
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2021-02-21 07:08:10 -0800

    daily commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,9 @@
 // IFFE
 (function () {
-    var d = new Date();
-    console.log(location, d);
+    var face = document.querySelector(".face");
+    var body = document.querySelector("body");
+    face.addEventListener("click", function (event) {
+        event.target.classList.toggle("spin");
+        body.classList.toggle("spin");
+    });
 })();
